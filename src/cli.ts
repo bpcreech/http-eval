96898199395c82b7b7f9d6eb7a48ecd554c0a55c
Ben Creech <hi@bpcreech.com>
2024-02-18 20:09:43 -0500

    Clean up server on exit

--- src/cli.ts
+++ src/cli.ts
@@ -1,10 +1,24 @@
 import { app } from "./http-eval.ts";
+import { existsSync } from "node:fs";
+import { unlink } from "node:fs/promises";
 
 async function getUdsPath() {
   if (!process.env.HTTP_EVAL_UDS_PATH) {
     throw new Error("You must define the variable HTTP_EVAL_UDS_PATH");
   }
-  return process.env.HTTP_EVAL_UDS_PATH!;
+
+  const path = process.env.HTTP_EVAL_UDS_PATH!;
+
+  if (existsSync(path)) {
+    throw new Error(
+      `Listen path ${path} already exists. You must remove it first.`,
+    );
+  }
+
+  return path;
 }
 
-app.listen(await getUdsPath());
+const server = app.listen(await getUdsPath());
+
+process.on("SIGTERM", () => server.close());
+process.on("SIGINT", () => server.close());
